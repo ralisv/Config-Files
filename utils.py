ef01420621b56f9d8dcc54b4d28723ef3333ad83
Vojtech Ralis <ralis.vojtech@email.cz>
2023-04-24 22:08:48 +0200

    Add dump-trash function

--- utils.py
+++ utils.py
@@ -12,6 +12,8 @@ init()
 STATUS_GOOD = 0
 STATUS_LITTLE_ERROR = 1
 STATUS_NO_ENTRIES = 2
+TRASH = os.path.expanduser("~/.trash-bin")
+RMLOG = os.path.expanduser("~/.rmlog.txt")
 
 
 def remove(args):
@@ -20,9 +22,8 @@ def remove(args):
         print(f"{Fore.RED}No files or directories passed{Fore.RESET}", file=sys.stderr)
         return STATUS_NO_ENTRIES
 
-    trash = os.path.expanduser("~/.trash-bin")
-    if not os.path.exists(trash):
-        os.mkdir(trash)
+    if not os.path.exists(TRASH):
+        os.mkdir(TRASH)
 
     for arg in args:
         arg = os.path.expanduser(arg)
@@ -31,15 +32,15 @@ def remove(args):
         for file in files:
             file_name = os.path.basename(file)
 
-            if os.path.exists(os.path.join(trash, file_name)):
+            if os.path.exists(os.path.join(TRASH, file_name)):
                 i = 1
-                while os.path.exists(os.path.join(trash, f"{file_name}_{i}")):
+                while os.path.exists(os.path.join(TRASH, f"{file_name}_{i}")):
                     i += 1
                 file_name = f"{file_name}_{i}"
 
-            shutil.move(file, os.path.join(trash, file_name))
+            shutil.move(file, os.path.join(TRASH, file_name))
 
-            with open(os.path.expanduser("~/.rmlog.txt"), "a") as f:
+            with open(RMLOG, "a") as f:
                 f.write(f"{time.strftime('%d. %m. %Y')} {file} moved to ~/.trash-bin/{file_name}\n")
 
         if not files:
@@ -47,6 +48,25 @@ def remove(args):
             print(f"{Fore.RED}{arg} does not match any files or directories{Fore.RESET}", file=sys.stderr)
 
     return status
+
+
+def dump_trash():
+    """ Permanently deletes all files in .trash-bin directory that haven't been modified in more than 30 days """
+    with open(RMLOG, "a") as f:
+        for file in glob.glob(f"{TRASH}/*"):
+            try:
+                if (time.time() - os.path.getmtime(file)) // (60 * 60 * 24) > 30:
+                    if os.path.isdir(file):
+                        shutil.rmtree(file)
+                    else:
+                        os.remove(file)
+
+                    f.write(f"{time.strftime('%d. %m. %Y')} {file} removed permanently\n")
+
+            except Exception as e:
+                f.write(f"{time.strftime('%d. %m. %Y')} {e}\n")
+
+
 
 
 def start_in_new_session(process, args, quiet=True):
