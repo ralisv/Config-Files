c91b7c180bf4aa545b1f0e265313ba1c419e9687
Vojtěch Ráliš <xralis@fi.muni.cz>
2023-06-11 18:15:15 +0200

    Refactor; fix coloring bug in remove; make colorize recognize more file types

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import shutil
 import sys
 import time
 import subprocess
+import stat
 
 from colorama import Fore, init
 from typing import List
@@ -11,8 +12,9 @@ from typing import List
 
 init()
 
-LS_COLORS = open("/home/ralis/Config-Files/ls-colors.txt").read()
+LS_COLORS = open(f"{os.path.expanduser('~')}/Config-Files/ls-colors.txt").read()
 """ The contents of the LS_COLORS environment variable """
+
 LS_COLORS_PARSED = dict(map(lambda assignment: assignment.split(sep="="), LS_COLORS.split(sep=":")))
 """ LS_COLORS parsed into a dictionary where the keys are file types and the values are color codes """
 
@@ -33,6 +35,8 @@ DELETED_FILE_AGE_LIMIT = 30
 """ Number of days after which the file is considered dumpable """
 
 
+import os
+
 def colorize(filename: str) -> str:
     """
     Returns the filename enclosed in the color escape sequence based on LS_COLORS
@@ -43,8 +47,10 @@ def colorize(filename: str) -> str:
     _, ext = os.path.splitext(filename)
     ext = f"*{ext}"
 
-    if ext in LS_COLORS_PARSED:
-        color = LS_COLORS_PARSED.get(ext)
+    color = None
+
+    if os.path.isfile(filename) and ext in LS_COLORS_PARSED:
+        color = LS_COLORS_PARSED[ext]
 
     # If the file is a directory, get the directory color
     elif os.path.isdir(filename):
@@ -54,20 +60,36 @@ def colorize(filename: str) -> str:
     elif os.path.islink(filename):
         color = LS_COLORS_PARSED.get('ln')
 
+    # If the file is a fifo pipe, get the pipe color
+    elif os.path.exists(filename) and stat.S_ISFIFO(os.stat(filename).st_mode):
+        color = LS_COLORS_PARSED.get('pi')
+
+    # If the file is a socket, get the socket color
+    elif os.path.exists(filename) and stat.S_ISSOCK(os.stat(filename).st_mode):
+        color = LS_COLORS_PARSED.get('so')
+
+    # If the file is a block (buffered) special file, get the block color
+    elif os.path.exists(filename) and stat.S_ISBLK(os.stat(filename).st_mode):
+        color = LS_COLORS_PARSED.get('bd')
+
+    # If the file is a character (unbuffered) special file, get the character color
+    elif os.path.exists(filename) and stat.S_ISCHR(os.stat(filename).st_mode):
+        color = LS_COLORS_PARSED.get('cd')
+
+    # If the file is a symbolic link and orphaned, get the orphan color
+    elif os.path.islink(filename) and not os.path.exists(os.readlink(filename)):
+        color = LS_COLORS_PARSED.get('or')
+
     # If the file is a regular file and an executable
     elif os.path.isfile(filename) and os.access(filename, os.X_OK):
         color = LS_COLORS_PARSED.get('ex')
 
-    else:
+    if color is None:
         color = LS_COLORS_PARSED.get('rs')
-
-    # The color sequence in LS_COLORS is a string like "38;2;r;g;b"
-    # Convert this to an ANSI escape sequence like "\033[...m"
-    color_sequence = f"\033[{color}m"
 
     # Return the filename enclosed in the color escape sequence
     # The "\033[0m" sequence at the end resets the color back to the default
-    return f"{color_sequence}{filename}\033[0m"
+    return f"\033[{color}m{filename}\033[0m"
 
 
 def remove(args: List[str]) -> int:
@@ -91,7 +113,7 @@ def remove(args: List[str]) -> int:
     if not os.path.exists(TRASH):
         os.mkdir(TRASH)
 
-    deleted = []
+    ok_messages = []
     error_messages = []
 
     for arg in args:
@@ -101,6 +123,7 @@ def remove(args: List[str]) -> int:
 
         for file in files:
             file_name = os.path.basename(file)
+            message = f"{Fore.GREEN} ✔ {colorize(os.path.basename(file_name))}{Fore.RESET}"
 
             if os.path.exists(os.path.join(TRASH, file_name)):
                 i = 1
@@ -110,25 +133,21 @@ def remove(args: List[str]) -> int:
 
             try:
                 shutil.move(file, os.path.join(TRASH, file_name))
-            except Exception as e:
+                ok_messages.append(message)
 
-                error_messages.append(f"{Fore.RED}Error while trying to remove {colorize(os.path.basename(file))}: {e}{Fore.RESET}")
+            except Exception as e:
+                message = f"{Fore.RED} ✘ {colorize(os.path.basename(file))}{Fore.RED}: {e}{Fore.RESET}"
+                error_messages.append(message)
 
                 status = STATUS_LITTLE_ERROR
-                continue
-
-            deleted.append(f"{colorize(os.path.basename(file))}")
 
         if not files:
             status = STATUS_LITTLE_ERROR
+            message = f"{Fore.RED} ✘ {arg}: Does not match any files or directories{Fore.RESET}"
+            error_messages.append(message)
 
-            error_messages.append(f"{Fore.RED}{arg} does not match any files or directories{Fore.RESET}")
-
-    if deleted:
-        print(*[f"{filename}{Fore.GREEN} was successfully deleted{Fore.RESET}" for filename in deleted], sep="\n")
-    
-    if error_messages:
-        print(*error_messages, sep="\n", file=sys.stderr)
+    print(*ok_messages, sep="\n", end="")
+    print(*error_messages, sep="\n", end="", file=sys.stderr)
 
     return status
 
@@ -176,9 +195,9 @@ def dump_trash(to_dump: List[os.DirEntry]) -> None:
             total_size += curr_size
 
         except Exception as e:
-            error_messages.append(f"{Fore.RED}An error occurred while attempting to delete {Fore.YELLOW}{entry.name}{Fore.RED}: {e}{Fore.RESET}")
+            message = f"{Fore.RED} ✘ {colorize(entry.path)}{Fore.RED}: {e}{Fore.RESET}"
 
-    print(*error_messages, sep="\n", file=sys.stderr)
+    print(*error_messages, sep="\n", end="", file=sys.stderr)
 
     return total_size
 
@@ -188,15 +207,17 @@ def ask_whether_to_dump() -> None:
     Asks the user whether to dump the trash or not,
     only asks if there are files that can be dumped and if the user hasn't been asked in the last 7 days
     """
-    if os.path.exists(DUMPLOG) and (time.time() - os.path.getmtime(DUMPLOG)) // (60 * 60 * 24) < 7 \
-            or not (dumpable := sorted(get_dumpable_files(DELETED_FILE_AGE_LIMIT), key=lambda entry: entry.name)):
+    if os.path.exists(DUMPLOG) and (time.time() - os.path.getmtime(DUMPLOG)) // (60 * 60 * 24) < 0:
         return
     
-    print(f"{Fore.GREEN}The following files have been in the trash for more than {DELETED_FILE_AGE_LIMIT} days:{Fore.RESET}")
-    for entry in dumpable:
-        print(f"{colorize(entry.name)}")
+    dumpable = sorted(get_dumpable_files(DELETED_FILE_AGE_LIMIT), key=lambda entry: entry.name)
+    if not dumpable:
+        return
 
-    print(f"{Fore.GREEN}Do you want to permanently delete them? [y/n] {Fore.RESET}", end="")
+    print(f"{Fore.GREEN}The following files have been in the trash for more than {DELETED_FILE_AGE_LIMIT} days:{Fore.RESET}", end="\n\n")
+    print(*[colorize(file.path).replace(os.path.expanduser("~/.trash-bin/"), "") for file in dumpable], sep="\n", end="\n\n")
+
+    print(f"{Fore.GREEN}Do you wish to permanently delete them? [y/n] {Fore.RESET}", end="")
     answer = input()
 
     with open(DUMPLOG, "a") as f:
