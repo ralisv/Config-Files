d6a2a6c9c3e74d6a7540f6c3995c4f2edf794304
Vojtěch Ráliš <ralis.vojtech@email.cz>
2023-09-01 00:05:31 +0200

    Add nixos configuration file

---
+++ configuration.nix
@@ -0,0 +1,127 @@
+{ config, lib, pkgs, ... }:
+
+{
+  security.sudo.wheelNeedsPassword = false;
+  
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      (let rev = "main"; in import (builtins.fetchTarball {
+        url = "https://gitlab.com/VandalByte/darkmatter-grub-theme/-/archive/${rev}/darkmatter-grub-theme-${rev}.tar.gz";
+        sha256 = "1i6dwmddjh0cbrp6zgafdrji202alkz52rjisx0hs1bgjbrbwxdj";
+        }))
+    ];
+
+  programs = {
+  	light = {
+  	  enable = true;
+  	};
+  	git = {
+  	  enable = true;
+  	};
+  };
+  
+  environment.systemPackages = with pkgs; [
+    brave
+    python3
+    python3Packages.gitpython
+    python3Packages.colorama
+    python3Packages.tabulate
+  	xonsh
+    vscode
+    nixos-grub2-theme
+  ];
+  
+  
+  # Bootloader.
+  boot.loader = {
+    # systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+    grub = {
+      enable = true;
+      version = 2;
+      device = "nodev";
+      useOSProber = true;
+      darkmatter-theme = {
+        enable = true;
+        style = "nixos";
+        icon = "color";
+        resolution = "1080p";
+      };
+      efiSupport = true;
+    };
+  };
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Prague";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "cs_CZ.UTF-8";
+    LC_IDENTIFICATION = "cs_CZ.UTF-8";
+    LC_MEASUREMENT = "cs_CZ.UTF-8";
+    LC_MONETARY = "cs_CZ.UTF-8";
+    LC_NAME = "cs_CZ.UTF-8";
+    LC_NUMERIC = "cs_CZ.UTF-8";
+    LC_PAPER = "cs_CZ.UTF-8";
+    LC_TELEPHONE = "cs_CZ.UTF-8";
+    LC_TIME = "cs_CZ.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+ 
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.ralis = {
+    isNormalUser = true;
+    description = "Vojtech Ralis";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  system.stateVersion = "23.05";
+}
