15552b6088cd8ecfe3f4a38c25125400c9a0d0bb
Vojtěch Ráliš <ralis.vojtech@email.cz>
2023-12-06 19:10:11 +0100

    Lower the garbage collection frequency

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -210,8 +210,8 @@
   };
 
   nix.gc.automatic = true;
-  nix.gc.dates = "weekly";
-  nix.gc.options = "--delete-older-than 7d";
+  nix.gc.dates = "monthly";
+  nix.gc.options = "--delete-older-than 30d";
 
   nix.optimise.automatic = true;
   nix.settings.auto-optimise-store = true;
