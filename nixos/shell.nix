e4721d2f31b95b5717a4364ea79af3e39dbca292
Vojtěch Ráliš <ralis.vojtech@email.cz>
2023-12-11 19:30:46 +0100

    Refactor file; add prompt toolkit library

--- nixos/shell.nix
+++ nixos/shell.nix
@@ -1,16 +1,17 @@
 # On NixOS, you can't install python packages globally, this nix expression must be executed in nix-shell
 # where my configuration of xonsh runs properly
 
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
 pkgs.mkShell {
-  buildInputs = with pkgs.python3Packages; [
-		gitpython
-		colorama
-		tabulate
+  buildInputs = with pkgs; [
+    python3Packages.gitpython
+    python3Packages.tabulate
+    python3Packages.wheel
+    python3Packages.prompt-toolkit
   ];
-	shellHook = ''
-		xonsh
+  shellHook = ''
+    xonsh
 		exit
-	'';
+  '';
 }
