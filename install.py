240f9cbcfcfc38371c767c5a7fd7127ce1664de5
Vojtěch Ráliš <ralis.vojtech@email.cz>
2024-01-11 12:13:18 +0000

    Add micro configuration files

--- install.py
+++ install.py
@@ -2,62 +2,61 @@
 
 
 import os
-import sys
 import platform
-
+import sys
 
 if platform.system() == "Linux":
-  print("Linux system detected")
+    print("Linux system detected")
 
+    INSTALL_SCRIPT_DIRECTORY = os.path.dirname(os.path.abspath(sys.argv[0]))
+    USER_HOME_DIRECTORY = os.path.expanduser("~")
 
-  INSTALL_SCRIPT_DIRECTORY = os.path.dirname(os.path.abspath(sys.argv[0]))
-  USER_HOME_DIRECTORY = os.path.expanduser("~")
+    SOURCE_TO_DESTINATION_SYMLINK_MAPPING = {
+        f"{INSTALL_SCRIPT_DIRECTORY}/.bashrc": f"{USER_HOME_DIRECTORY}/.bashrc",
+        f"{INSTALL_SCRIPT_DIRECTORY}/xonsh/.xonshrc": f"{USER_HOME_DIRECTORY}/.xonshrc",
+        f"{INSTALL_SCRIPT_DIRECTORY}/.inputrc": f"{USER_HOME_DIRECTORY}/.inputrc",
+        f"{INSTALL_SCRIPT_DIRECTORY}/vs-code/settings.json": f"{USER_HOME_DIRECTORY}/.config/Code/User/settings.json",
+        f"{INSTALL_SCRIPT_DIRECTORY}/vs-code/keybindings.json": f"{USER_HOME_DIRECTORY}/.config/Code/User/keybindings.json",
+        f"{INSTALL_SCRIPT_DIRECTORY}/alacritty.yml": f"{USER_HOME_DIRECTORY}/.config/alacritty/alacritty.yml",
+        f"{INSTALL_SCRIPT_DIRECTORY}/nixos/shell.nix": f"{USER_HOME_DIRECTORY}/shell.nix",
+        f"{INSTALL_SCRIPT_DIRECTORY}/nixos/flake.nix": f"{USER_HOME_DIRECTORY}/flake.nix",
+        f"{INSTALL_SCRIPT_DIRECTORY}/micro/settings.json": f"{USER_HOME_DIRECTORY}/.config/micro/settings.json",
+        f"{INSTALL_SCRIPT_DIRECTORY}/micro/bindings.json": f"{USER_HOME_DIRECTORY}/.config/micro/bindings.json",
+    }
 
+    for source, destination in SOURCE_TO_DESTINATION_SYMLINK_MAPPING.items():
+        if not os.path.exists(source):
+            print(f"Source path {source} does not exist")
+            continue
 
-  SOURCE_TO_DESTINATION_SYMLINK_MAPPING = {
-    f"{INSTALL_SCRIPT_DIRECTORY}/.bashrc": f"{USER_HOME_DIRECTORY}/.bashrc",
-    f"{INSTALL_SCRIPT_DIRECTORY}/xonsh/.xonshrc": f"{USER_HOME_DIRECTORY}/.xonshrc",
-    f"{INSTALL_SCRIPT_DIRECTORY}/.inputrc": f"{USER_HOME_DIRECTORY}/.inputrc",
-    f"{INSTALL_SCRIPT_DIRECTORY}/vs-code/settings.json": f"{USER_HOME_DIRECTORY}/.config/Code/User/settings.json",
-    f"{INSTALL_SCRIPT_DIRECTORY}/vs-code/keybindings.json": f"{USER_HOME_DIRECTORY}/.config/Code/User/keybindings.json",
-    f"{INSTALL_SCRIPT_DIRECTORY}/alacritty.yml": f"{USER_HOME_DIRECTORY}/.config/alacritty/alacritty.yml",
-    f"{INSTALL_SCRIPT_DIRECTORY}/nixos/shell.nix": f"{USER_HOME_DIRECTORY}/shell.nix",
-    f"{INSTALL_SCRIPT_DIRECTORY}/nixos/flake.nix": f"{USER_HOME_DIRECTORY}/flake.nix",
-  }
+        if os.path.exists(destination):
+            print(f"Path {destination} already exists")
+            continue
 
+        if not os.path.exists(os.path.dirname(destination)):
+            print(f"Creating directory {os.path.dirname(destination)}")
+            os.makedirs(os.path.dirname(destination))
 
-  for (source, destination) in SOURCE_TO_DESTINATION_SYMLINK_MAPPING.items():
-    if not os.path.exists(source):
-      print(f"Source path {source} does not exist")
-      continue
+        print(f"Creating symlink {destination} -> {source}")
+        os.symlink(source, destination)
 
-    if os.path.exists(destination):
-      print(f"Path {destination} already exists")
-      continue
+    def update_nix_configuration():
+        try:
+            if input("Do you wish to update configuration.nix file? ") in ("y", ""):
+                os.system(
+                    f"sudo cp {INSTALL_SCRIPT_DIRECTORY}/nixos/configuration.nix /etc/nixos/configuration.nix"
+                )
+            else:
+                return
 
-    if not os.path.exists(os.path.dirname(destination)):
-      print(f"Creating directory {os.path.dirname(destination)}")
-      os.makedirs(os.path.dirname(destination))
+            if input("Do you wish to test new nixos configuration? ") in ("y", ""):
+                os.system("sudo nixos-rebuild test")
 
-    print(f"Creating symlink {destination} -> {source}")
-    os.symlink(source, destination)
+            if input("Do you wish to switch to new nixos configuration? ") in ("y", ""):
+                os.system("sudo nixos-rebuild test")
 
+        except KeyboardInterrupt:
+            print()
 
-  def update_nix_configuration():
-    try:
-      if (input("Do you wish to update configuration.nix file? ") in ("y", "")):
-        os.system(f"sudo cp {INSTALL_SCRIPT_DIRECTORY}/nixos/configuration.nix /etc/nixos/configuration.nix")
-      else:
-        return
-      
-      if (input("Do you wish to test new nixos configuration? ") in ("y", "")):
-        os.system("sudo nixos-rebuild test")
-
-      if (input("Do you wish to switch to new nixos configuration? ") in ("y", "")):
-        os.system("sudo nixos-rebuild test")
-
-    except KeyboardInterrupt:
-      print()
-
-  if os.path.exists("/etc/nixos"):
-    update_nix_configuration()
+    if os.path.exists("/etc/nixos"):
+        update_nix_configuration()
