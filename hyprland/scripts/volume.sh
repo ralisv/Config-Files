328ba7fce50a756eea7f5afc0e10cbf2754d0df9
Vojtěch Ráliš <ralis.vojtech@email.cz>
2024-02-05 15:13:15 +0100

    Improve the volume script

--- hyprland/scripts/volume.sh
+++ hyprland/scripts/volume.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 
-if [ $1 = "up" ]; then wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+;
-elif [ $1 = "down" ]; then wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%-;
-elif [ $1 = "mute" ]; then wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle;
+if [ -z "$1" ]; then
+    echo "Usage: $0 {up|down|mute}"
+    exit 1
 fi
+
+if [ "$1" = "up" ]; then 
+    wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+
+elif [ "$1" = "down" ]; then 
+    wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
+elif [ "$1" = "mute" ]; then 
+    wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+else
+    echo "Invalid argument: $1"
+    echo "Usage: $0 {up|down|mute}"
+    exit 1
+fi
+
 
 export SOUND_SETTINGS_CACHE=`wpctl get-volume @DEFAULT_AUDIO_SINK@`
 eww --config ~/Config-Files/hyprland/eww update "sound-settings=$SOUND_SETTINGS_CACHE"
