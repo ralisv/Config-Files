c81ae8f4c8151b02ddf3abe6c24620f28ffa95e0
Vojtěch Ráliš <ralis.vojtech@email.cz>
2024-02-10 16:14:12 +0100

    Fix wallpaper-manager

--- hyprland/scripts/wallpaper-manager.sh
+++ hyprland/scripts/wallpaper-manager.sh
@@ -3,7 +3,11 @@
 sleep 1  # Wait a while to have a good look at the hyprland logo
 swww init
 
+# Get the directory of the current script
+SCRIPT_DIR=$(dirname "$0")
+
 while true; do
-    ./pick-random-wallpaper.sh
+    # Call the pick-random-wallpaper.sh script using the determined script directory
+    "$SCRIPT_DIR/pick-random-wallpaper.sh"
     sleep 1800
 done
