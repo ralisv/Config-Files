101222889cad9ffcfd443c18e1c1eaad95d25d2d
ralis <ralis.vojtech@email.cz>
2024-03-13 07:01:08 +0000

    Attempt setting gtk theme in home manager

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,6 +1,28 @@
 { config, pkgs, ... }:
 
 {
+  home.file.".icons/default".source = "${pkgs.vanilla-dmz}/share/icons/Vanilla-DMZ";
+  home.pointerCursor =
+    let
+      getFrom = url: hash: name: {
+        gtk.enable = true;
+        name = name;
+        size = 48;
+        package =
+          pkgs.runCommand "moveUp" { } ''
+                          mkdir -p $out/share/icons
+                          ln -s ${pkgs.fetchzip {
+            url = url;
+            hash = hash;
+            }} $out/share/icons/${name}
+          '';
+      };
+    in
+    getFrom
+      "https://github.com/ful1e5/fuchsia-cursor/releases/download/v2.0.0/Fuchsia-Pop.tar.gz"
+      "sha256-BvVE9qupMjw7JRqFUj1J0a4ys6kc9fOLBPx2bGaapTk="
+      "Fuchsia-Pop";
+
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "ralis";
