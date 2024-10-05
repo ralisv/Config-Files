58aebcee4412dc40ab339cc55d8e92a0cb928d8c
ralis <ralis.vojtech@email.cz>
2024-10-05 16:55:24 +0000

    Update xonsh; rework nix shell files

--- shell/nix/shell.nix
+++ shell/nix/shell.nix
@@ -1,36 +1,20 @@
-# On NixOS, you can't install python packages globally, this nix expression must be executed in nix-shell
-# where my configuration of xonsh runs properly
-
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs }:
 
 let
-  my-python-packages = p: with p; [
-    (buildPythonPackage rec {
-      pname = "xonsh";
-      version = "0.15.1";
-
-      src = fetchPypi {
-        inherit pname version;
-        sha256 = "sha256-NKYzK3qG9v6Gp0JzWFxZqx88iSkv2lqer+VMkmusRxA=";
-      };
-
-      doCheck = false;
-    })
-  ];
+  python-with-packages = pkgs.python312.withPackages (ps: with ps; [
+    tabulate
+    types-tabulate
+    wheel
+    prompt-toolkit
+    pygments
+    xonsh
+  ]);
 in
 pkgs.mkShell {
-  nativeBuildInputs = [
-    (pkgs.python312.withPackages my-python-packages)
-  ];
-  buildInputs = with pkgs; [
-    python312Packages.tabulate
-    python312Packages.types-tabulate
-    python312Packages.wheel
-    python312Packages.prompt-toolkit
-    python312Packages.pygments
+  buildInputs = [
+    python-with-packages
   ];
   shellHook = ''
-    python3.12 -m xonsh
-    exit
+    exec ${python-with-packages}/bin/xonsh
   '';
 }
