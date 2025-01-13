ffda91e718a66e7afac5a3ea44c1e2f11b582359
ralis <ralis.vojtech@email.cz>
2025-01-13 23:37:32 +0100

    Reapply "Add hyprpanel"
    This reverts commit b74889f9b0a543261085cc04f6dfb00704f3bbd8.

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -5,18 +5,25 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/master";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs"; # Use the same nixpkgs as the main input
+    };
+    hyprpanel = {
+      url = "github:jas-singhfsu/hyprpanel";
+      inputs.nixpkgs.follows = "nixpkgs"; # Use the same nixpkgs as the main input
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs, home-manager, hyprpanel, ... }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
     in
     {
       homeConfigurations."ralis" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+        extraSpecialArgs = { inherit hyprpanel; inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
         modules = [ ./home.nix ];
       };
     };
