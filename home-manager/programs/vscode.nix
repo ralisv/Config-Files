a41566f55a51416a3dcd7c4cdd9c41f1e8ae6867
ralis <ralis.vojtech@email.cz>
2025-01-20 22:44:32 +0100

    Set vscode as default app for some files

--- home-manager/programs/vscode.nix
+++ home-manager/programs/vscode.nix
@@ -44,4 +44,53 @@
       }
     ];
   };
+
+  xdg.mimeApps = {
+    defaultApplications = {
+      # Plain text and documents
+      "text/plain" = [ "code.desktop" ];
+      "text/markdown" = [ "code.desktop" ];
+      "text/x-markdown" = [ "code.desktop" ];
+      "text/x-log" = [ "code.desktop" ];
+
+      # Markup languages
+      "text/html" = [ "code.desktop" ];
+      "text/xml" = [ "code.desktop" ];
+      "application/xml" = [ "code.desktop" ];
+      "application/json" = [ "code.desktop" ];
+      "application/ld+json" = [ "code.desktop" ];
+      "application/x-yaml" = [ "code.desktop" ];
+      "text/yaml" = [ "code.desktop" ];
+      "text/x-toml" = [ "code.desktop" ];
+
+      # Common programming languages
+      "text/x-python" = [ "code.desktop" ];
+      "text/x-python3" = [ "code.desktop" ];
+      "text/x-java" = [ "code.desktop" ];
+      "text/x-c" = [ "code.desktop" ];
+      "text/x-c++" = [ "code.desktop" ];
+      "text/x-c++src" = [ "code.desktop" ];
+      "text/x-csrc" = [ "code.desktop" ];
+      "text/javascript" = [ "code.desktop" ];
+      "application/javascript" = [ "code.desktop" ];
+      "application/x-javascript" = [ "code.desktop" ];
+      "text/x-typescript" = [ "code.desktop" ];
+      "text/typescript" = [ "code.desktop" ];
+      "text/x-php" = [ "code.desktop" ];
+      "application/x-php" = [ "code.desktop" ];
+      "text/x-ruby" = [ "code.desktop" ];
+      "text/x-shellscript" = [ "code.desktop" ];
+      "application/x-shellscript" = [ "code.desktop" ];
+      "text/x-makefile" = [ "code.desktop" ];
+      "text/x-rust" = [ "code.desktop" ];
+      "text/x-go" = [ "code.desktop" ];
+
+      # Configuration files
+      "text/x-properties" = [ "code.desktop" ];
+      "text/x-config" = [ "code.desktop" ];
+      "application/x-desktop" = [ "code.desktop" ];
+      "application/x-perl" = [ "code.desktop" ];
+    };
+  };
+
 }
