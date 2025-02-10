ca6595927eb647cae40900ffe3cbb3331d8f1ab7
ralis <ralis.vojtech@email.cz>
2025-02-10 09:24:55 +0100

    Refactor newsboat config

--- home-manager/programs/newsboat.nix
+++ home-manager/programs/newsboat.nix
@@ -1,22 +1,22 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    newsboat
-  ];
+  programs.newsboat = {
+    enable = true;
+    reloadThreads = 12;
+    reloadTime = 120;
+    extraConfig = ''
+      keep-articles-days 365
+      article-sort-order date-asc
+      prepopulate-query-feeds yes
 
-  home.file.".newsboat/config".text = ''
-    keep-articles-days 365
-    article-sort-order date-asc
-    prepopulate-query-feeds yes
-    refresh-on-startup yes
-
-    color background default default
-    color listnormal color249 default
-    color listfocus default default standout
-    color listnormal_unread yellow default
-    color listfocus_unread default default standout
-    color info default default bold
-    color article default default
-  '';
+      color background default default
+      color listnormal color249 default
+      color listfocus default default standout
+      color listnormal_unread color148 default
+      color listfocus_unread default default standout
+      color info color208 default bold
+      color article color253 default
+    '';
+  };
 }
